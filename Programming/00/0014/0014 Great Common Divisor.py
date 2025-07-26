e69494d8d90a1731ecf74402350b1fca5ac46c66
Worralop Srichainont <worralop.sric@gmail.com>
2025-07-27 00:43:10 +0700

    feat: add 0014 solution

--- Programming/00/0014/0014 Great Common Divisor.py
+++
@@ -1,8 +0,0 @@
-# Import math library
-import math
-
-# Input 2 number
-number = [int(e) for e in input().split()]
-
-# Output GCD
-print(math.gcd(number[0],number[1]))
---
+++ Programming/00/0014/0014.py
@@ -0,0 +1,11 @@
+# --------------------------------------------------
+# File Name : 0014.py
+# Problem   : Great Common Divisor
+# Author    : Worralop Srichainont
+# Date      : 2025-07-26
+# --------------------------------------------------
+
+import math
+
+a, b = map(int, input().split())
+print(math.gcd(a, b))
