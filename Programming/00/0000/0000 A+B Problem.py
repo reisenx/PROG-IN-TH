80d15a6012f0eb02fac29d4cf864cea1078924d8
Worralop Srichainont <worralop.sric@gmail.com>
2025-07-26 22:53:40 +0700

    feat: add 0000 solution

--- Programming/00/0000/0000 A+B Problem.py
+++
@@ -1,6 +0,0 @@
-# Input integers
-a = int(input())
-b = int(input())
-
-# Output a value of a+b
-print(a+b)
---
+++ Programming/00/0000/0000.py
@@ -0,0 +1,13 @@
+# --------------------------------------------------
+# File Name : 0000.py
+# Problem   : A + B Problem
+# Author    : Worralop Srichainont
+# Date      : 2025-07-26
+# --------------------------------------------------
+
+# Input two integers
+a = int(input())
+b = int(input())
+
+# Output the value of a + b
+print(a + b)
