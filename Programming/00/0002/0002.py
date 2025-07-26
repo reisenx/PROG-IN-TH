9b3779c2ea62b76390713fd59590b728d603457d
Worralop Srichainont <worralop.sric@gmail.com>
2025-07-27 00:39:36 +0700

    feat: add 0002 solution

---
+++ Programming/00/0002/0002.py
@@ -0,0 +1,16 @@
+# --------------------------------------------------
+# File Name : 0002.py
+# Problem   : Min Max
+# Author    : Worralop Srichainont
+# Date      : 2025-07-26
+# --------------------------------------------------
+
+# Input numbers
+numbers = []
+n = int(input())
+for _ in range(n):
+    numbers.append(int(input()))
+
+# Output min and max of the number list
+print(min(numbers))
+print(max(numbers))
