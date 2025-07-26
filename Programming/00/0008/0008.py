9a3aceb531f099ec05e1e77b599f60c650d464e7
Worralop Srichainont <worralop.sric@gmail.com>
2025-07-27 00:40:20 +0700

    feat: add 0008 solution

---
+++ Programming/00/0008/0008.py
@@ -0,0 +1,13 @@
+# --------------------------------------------------
+# File Name : 0008.py
+# Problem   : X2
+# Author    : Worralop Srichainont
+# Date      : 2025-07-26
+# --------------------------------------------------
+
+# Input x1 and average of x1 and x2.
+x1, average = map(int, input().split())
+
+# Calculate and output x2.
+x2 = (2 * average) - x1
+print(x2)
